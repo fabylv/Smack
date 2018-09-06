21556702e09115b1046e5eb3261182722178d2ca
Faby <fabylv@gmail.com>
2018-09-06 16:01:47 -0700

    auth service

--- app/src/main/java/com/example/faby/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/faby/smack/Controller/LoginActivity.kt
@@ -1,9 +1,10 @@
-package com.example.faby.smack
+package com.example.faby.smack.Controller
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.faby.smack.R
 
 class LoginActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/example/faby/smack/Services/AuthService.kt
@@ -0,0 +1,5 @@
+package com.example.faby.smack.Services
+
+object AuthService {
+    
+}
