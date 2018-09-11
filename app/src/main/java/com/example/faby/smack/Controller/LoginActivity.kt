88919db5308a877cc5a07019fb78ff36b6eaa00e
Faby <fabylv@gmail.com>
2018-09-10 18:00:57 -0700

    login

--- app/src/main/java/com/example/faby/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/faby/smack/Controller/LoginActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.faby.smack.R
+import com.example.faby.smack.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,7 +16,20 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View) {
+        val email = loginEmailText.text.toString()
+        val password = loginPasswordText.text.toString()
 
+        AuthService.loginUser(this, email, password) { loginSuccess ->
+            if(loginSuccess) {
+                AuthService.findUserByEmail(this) { findSucess ->
+                    if(findSucess) {
+                        finish()
+                    }
+
+                }
+            }
+
+        }
     }
 
     fun loginCreateUserBtnClicked(view: View) {
--- app/src/main/java/com/example/faby/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/faby/smack/Controller/MainActivity.kt
@@ -59,9 +59,8 @@ class MainActivity : AppCompatActivity(){
     fun loginBtnNavClicked(view: View) {
 
         if(AuthService.isLoggedIn){
-            //logout
             UserDataService.logout()
-            userNameNavHeader.text = "Login"
+            userNameNavHeader.text = ""
             userEmailNavHeader.text = ""
             userImageNavHeader.setImageResource(R.drawable.profiledefault)
             userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
