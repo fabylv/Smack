d62fa74dc245cc300b7b0eeeb2bb7fffcf056375
Faby <fabylv@gmail.com>
2018-09-12 11:04:08 -0700

    message adapter

--- app/src/main/java/com/example/faby/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/faby/smack/Controller/MainActivity.kt
@@ -8,10 +8,12 @@ import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import com.example.faby.smack.Adapters.MessageAdapter
 import com.example.faby.smack.Model.Channel
 import com.example.faby.smack.Model.Message
 import com.example.faby.smack.R
@@ -32,11 +34,17 @@ class MainActivity : AppCompatActivity(){
     val socket = IO.socket(SOCKET_URL)
 
     lateinit var channelAdapter: ArrayAdapter<Channel>
+    lateinit var messageAdapter: MessageAdapter
     var selectedChannel : Channel? = null
 
     private fun setupAdapters() {
         channelAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, MessageService.channels)
         channel_list.adapter = channelAdapter
+
+        messageAdapter = MessageAdapter(this, MessageService.messages)
+        messageListView.adapter = messageAdapter
+        val layoutManager = LinearLayoutManager(this)
+        messageListView.layoutManager = layoutManager
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -106,8 +114,9 @@ class MainActivity : AppCompatActivity(){
         if(selectedChannel != null){
             MessageService.getMessages(selectedChannel!!.id) { complete ->
                 if(complete) {
-                    for(message in MessageService.messages) {
-                        println(message.message)
+                    messageAdapter.notifyDataSetChanged()
+                    if(messageAdapter.itemCount > 0) {
+                        messageListView.smoothScrollToPosition(messageAdapter.itemCount - 1)
                     }
                 }
             }
@@ -126,6 +135,8 @@ class MainActivity : AppCompatActivity(){
 
         if(App.prefs.isLoggedIn){
             UserDataService.logout()
+            channelAdapter.notifyDataSetChanged()
+            messageAdapter.notifyDataSetChanged()
             userNameNavHeader.text = ""
             userEmailNavHeader.text = ""
             userImageNavHeader.setImageResource(R.drawable.profiledefault)
@@ -192,6 +203,8 @@ class MainActivity : AppCompatActivity(){
 
                     val newMessage = Message(msgBody, userName, channelId, userAvatar, userAvatarColor, id ,timeStamp)
                     MessageService.messages.add(newMessage)
+                    messageAdapter.notifyDataSetChanged()
+                    messageListView.smoothScrollToPosition(messageAdapter.itemCount - 1)
                 }
             }
         }
