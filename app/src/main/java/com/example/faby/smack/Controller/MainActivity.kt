504b904e34ac5f9e4d0a57ee2fc0b5905ce08bd0
Faby <fabylv@gmail.com>
2018-09-12 09:21:00 -0700

    channels

--- app/src/main/java/com/example/faby/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/faby/smack/Controller/MainActivity.kt
@@ -23,6 +23,7 @@ import io.socket.client.IO
 import io.socket.emitter.Emitter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity(){
@@ -30,6 +31,7 @@ class MainActivity : AppCompatActivity(){
     val socket = IO.socket(SOCKET_URL)
 
     lateinit var channelAdapter: ArrayAdapter<Channel>
+    var selectedChannel : Channel? = null
 
     private fun setupAdapters() {
         channelAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, MessageService.channels)
@@ -49,6 +51,12 @@ class MainActivity : AppCompatActivity(){
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
         setupAdapters()
+
+        channel_list.setOnItemClickListener { _, _, position, id ->
+            selectedChannel = MessageService.channels[position]
+            drawer_layout.closeDrawer(GravityCompat.START)
+            updateWithChannel()
+        }
 
         if(App.prefs.isLoggedIn){
             AuthService.findUserByEmail(this){}
@@ -76,14 +84,23 @@ class MainActivity : AppCompatActivity(){
                 userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
                 loginBtnNavHeader.text = "Logout"
 
-                MessageService.getChannels(context) { complete ->
+                MessageService.getChannels { complete ->
                     if(complete){
-                        channelAdapter.notifyDataSetChanged()
+                        if(MessageService.channels.count() > 0){
+                            selectedChannel = MessageService.channels[0]
+                            channelAdapter.notifyDataSetChanged()
+                            updateWithChannel()
+                        }
                     }
-
                 }
             }
         }
+    }
+
+    fun updateWithChannel(){
+        mainChannelName.text = "#${selectedChannel?.name}"
+
+        //download msg for channel
     }
 
     override fun onBackPressed() {
@@ -118,7 +135,7 @@ class MainActivity : AppCompatActivity(){
             val dialogView = layoutInflater.inflate(R.layout.add_channel_dialog, null)
 
             builder.setView(dialogView)
-                    .setPositiveButton("Add") { dialog: DialogInterface?, which: Int ->
+                    .setPositiveButton("Add") { _, _ ->
                         val nameTextField = dialogView.findViewById<EditText>(R.id.addChannelNameTxt)
                         val descTextField = dialogView.findViewById<EditText>(R.id.addChannelDescTxt)
                         val channelName = nameTextField.text.toString()
@@ -127,7 +144,7 @@ class MainActivity : AppCompatActivity(){
                         socket.emit("newChannel", channelName, channelDesc)
 
                     }
-                    .setNegativeButton("Cancel") { dialog: DialogInterface?, which: Int ->
+                    .setNegativeButton("Cancel") { _, _ ->
 
                     }
                     .show()
--- app/src/main/java/com/example/faby/smack/Services/AuthService.kt
+++ app/src/main/java/com/example/faby/smack/Services/AuthService.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.example.faby.smack.Controller.App
 import com.example.faby.smack.Utilities.*
 import org.json.JSONException
@@ -15,7 +14,7 @@ import org.json.JSONObject
 
 object AuthService {
 
-    fun registerUser(context: Context, email: String, password: String, complete:(Boolean) -> Unit ) {
+    fun registerUser(email: String, password: String, complete:(Boolean) -> Unit ) {
 
         val jsonBody = JSONObject()
         jsonBody.put("email", email)
@@ -45,7 +44,7 @@ object AuthService {
     }
 
 
-    fun loginUser(context: Context, email: String, password: String, complete:(Boolean) -> Unit) {
+    fun loginUser(email: String, password: String, complete:(Boolean) -> Unit) {
 
         val jsonBody = JSONObject()
         jsonBody.put("email", email)
@@ -84,7 +83,7 @@ object AuthService {
         App.prefs.requestQueue.add(loginRequest)
     }
 
-    fun createUser(context: Context, name: String, email: String, avatarName: String, avatarColor: String, complete:(Boolean) -> Unit){
+    fun createUser(name: String, email: String, avatarName: String, avatarColor: String, complete:(Boolean) -> Unit){
 
         val jsonBody = JSONObject()
         jsonBody.put("name", name)
