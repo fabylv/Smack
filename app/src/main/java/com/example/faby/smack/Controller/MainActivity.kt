aeb25da791c8253859ff103ba8f41db59352d93d
Faby <fabylv@gmail.com>
2018-09-12 09:42:55 -0700

    new messages

--- app/src/main/java/com/example/faby/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/faby/smack/Controller/MainActivity.kt
@@ -13,6 +13,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import com.example.faby.smack.Model.Channel
+import com.example.faby.smack.Model.Message
 import com.example.faby.smack.R
 import com.example.faby.smack.Services.AuthService
 import com.example.faby.smack.Services.MessageService
@@ -45,6 +46,8 @@ class MainActivity : AppCompatActivity(){
 
         socket.connect()
         socket.on("channelCreated", onNewChannel)
+
+        socket.on("messageCreated", onNewMessage)
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -165,8 +168,30 @@ class MainActivity : AppCompatActivity(){
 
     }
 
+    private val onNewMessage = Emitter.Listener { args ->
+        runOnUiThread {
+            val msgBody = args[0] as String
+            val channelId = args[2] as String
+            val userName = args[3] as String
+            val userAvatar = args[4] as String
+            val userAvatarColor = args[5] as String
+            val id = args[6] as String
+            val timeStamp = args[7] as String
+
+            val newMessage = Message(msgBody, userName, channelId, userAvatar, userAvatarColor, id ,timeStamp)
+            MessageService.messages.add(newMessage)
+            //println(newMessage.message)
+        }
+    }
+
     fun sendMsgBtnClicked(view: View) {
-        hideKeyboard()
+        if(App.prefs.isLoggedIn && messageTextField.text.isNotEmpty() && selectedChannel != null){
+            val userId = UserDataService.id
+            val channelId = selectedChannel!!.id
+            socket.emit("newMessage", messageTextField.text.toString(), userId, channelId, UserDataService.name, UserDataService.avatarName, UserDataService.avatarColor)
+            messageTextField.text.clear()
+            hideKeyboard()
+        }
     }
 
     fun hideKeyboard(){
